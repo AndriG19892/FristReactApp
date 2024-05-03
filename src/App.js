1720d8d1220bb253746a9cfb123f13b9372e6893
AndreaG <andreag19892@gmail.com>
2024-05-03 21:09:57 +0200

    add spread operator for pass props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import Componente1 from './Componente1';
 import Prodotto from './Product';
 
+
+const primaCard = {
+  nome: '10€ Amazon',
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f7191.jpg',
+  prezzo: 2.16,
+  costoFinale: 6.99,
+};
+
+const secondaCard = {
+  nome: 'Mascherine 50pz',
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f7191.jpg',
+  prezzo: 8,
+  costoFinale: 10.99,
+};
+
 function App() {
   return (
     <div className="App">
       <h2>La Nostra Prima Card</h2>
-      <Prodotto />
-      <Prodotto />
-      <Prodotto />
-      <Prodotto />
-      {/*<Componente1 />*/}
+      <section className='card-section'>
+        {/*
+        invece che passare tutti i parametri uno per uno 
+        passo i parametri in questa forma ...[nome_dell_oggetto]
+        in questo modo sto passando tutti i parametri senza però elencarli tutti
+        */}
+        <Prodotto {...primaCard} />
+        <Prodotto {...secondaCard} />
+      </section>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const alt = 'valore';
-const img = 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f7191.jpg'
-const prezzo = '3,20';
-const pStyle = {
-    textTransform: "uppercase",
-};
-const Product = () => {
+const Prodotto = (props) => {
+    const alt = 'valore';
+    const img = 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f7191.jpg'
+    console.log(props.numero);
     return (
-        <article>
-            <div className='card'>
-                <img src={img} alt={alt} />
-                <h3 style={pStyle}>50€ Carburante + 120P</h3>
-                <div className='spacer'></div>
-                <h2 className='price'>{2 + 2.16} €</h2>
-                <h2 className='time'>00:10</h2>
-                <div className='puntata'>
-                    <p>punta</p>
-                </div>
-            </div>
-
-        </article>
+      <article>
+        <div className='card'>
+          <img src={props.img} alt={alt} />
+          <h3>{props.nome}</h3>
+          <div className='spacer'></div>
+          <h2 className='price'>{props.prezzo}€</h2>
+          <h2 className='time'>00:10</h2>
+          <div className='puntata'>
+            <p>{props.costoFinale.toString()}</p>
+          </div>
+        </div>
+  
+      </article>
     )
-}
+  }
 
-export default Product
+  export default Prodotto;
